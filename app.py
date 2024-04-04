8768c7fd825d1ad0530daa400fd3077110630909
Brian Ku <brian950804@gmail.com>
2024-04-04 22:20:27 +0800

    測試登革熱回傳功能

--- app.py
+++ app.py
@@ -56,13 +56,10 @@ def handle_message(event):
         if event.message.text == "登革熱":
             bot = urllib.request.urlopen("https://khweb.geohealth.tw/")
             web_content = bot.read().decode('utf8')
-            return_content = ''
-            for each_find in re.findall(r"<h5.+?>(\w+?)<span.+?>(\w\w)", web_content):
-                return_content = return_content + each_find[0] + each_find[1] + "\n"
             line_bot_api.reply_message_with_http_info(
                 ReplyMessageRequest(
                     reply_token=event.reply_token,
-                    messages=[TextMessage(text=return_content)]
+                    messages=[TextMessage(text='\n'.join(re.findall(r"<h5.+?>(\w+?)<span.+?>(\w+?)</span>", web_content)))]
                 )
             )
 
